18a0d36780ce15e8b7ede7361838e90110a5f867
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-13 14:41:28 -0700

    Verified that each new entry receives an incrementing entryId

--- js/data.js
+++ js/data.js
@@ -7,52 +7,16 @@ var data = {
   nextEntryId: 1
 };
 
-var imageUrlElement = document.querySelector('#photo-url');
-var image = document.querySelector('img');
-var $form = document.querySelector('form');
+var oldEntryJSON = localStorage.getItem('code-journal-local-storage');
 
-function uploadImage(event) {
-  var imageUrl = event.target.value;
-  image.setAttribute('src', imageUrl);
-}
-
-imageUrlElement.addEventListener('input', uploadImage);
-
-var dataObject = {};
-function saveEntry(event) {
-  event.preventDefault();
-  dataObject.title = $form.elements.title.value;
-  dataObject.photo = $form.elements.photo.value;
-  dataObject.notes = $form.elements.notes.value;
-  dataObject.nextEntryId = data.nextEntryId++;
-  data.entries.unshift(dataObject);
-  image.setAttribute('src', 'images/placeholder-image-square.jpg');
-  var newEntry = JSON.stringify(dataObject);
-  localStorage.setItem('new-entry', newEntry);
-  var dataModel = JSON.stringify(data);
-  localStorage.setItem('data-model', dataModel);
-  $form.reset();
-  // console.log(dataObject);
-  // console.log(data);
-}
-
-var newEntryJSON = localStorage.getItem('new-entry');
-var newDataJSON = localStorage.getItem('data-model');
-
-if (newEntryJSON !== null) {
-  dataObject = JSON.parse(newEntryJSON);
-}
-
-if (newDataJSON !== null) {
-  data = JSON.parse(newDataJSON);
+if (oldEntryJSON !== null) {
+  var oldData = JSON.parse(oldEntryJSON);
+  data = oldData;
 }
 
 function saveData(event) {
-  var newEntry = JSON.stringify(dataObject);
-  localStorage.setItem('new-entry', newEntry);
-  var dataModel = JSON.stringify(data);
-  localStorage.setItem('data-model', dataModel);
+  var newEntry = JSON.stringify(data);
+  localStorage.setItem('code-journal-local-storage', newEntry);
 }
 
 window.addEventListener('beforeunload', saveData);
-$form.addEventListener('submit', saveEntry);
--- js/main.js
+++ js/main.js
@@ -1,2 +1,31 @@
 /* global data */
 /* exported data */
+
+var imageUrlElement = document.querySelector('#photo-url');
+var image = document.querySelector('img');
+var $form = document.querySelector('form');
+
+function uploadImage(event) {
+  var imageUrl = event.target.value;
+  image.setAttribute('src', imageUrl);
+}
+
+imageUrlElement.addEventListener('input', uploadImage);
+
+function saveEntry(event) {
+  event.preventDefault();
+  var dataObject = {
+    title: $form.elements.title.value,
+    photo: $form.elements.photo.value,
+    notes: $form.elements.notes.value
+  };
+  dataObject.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(dataObject);
+  image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+  // console.log(dataObject);
+  // console.log(data);
+}
+
+$form.addEventListener('submit', saveEntry);
