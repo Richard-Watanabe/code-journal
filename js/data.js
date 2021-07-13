970c94b72e27ab470ba939555ea9de133a0a97e1
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-13 11:09:25 -0700

    Verified that new entries are saved to local storage along with the rest of the data model on page reload

--- js/data.js
+++ js/data.js
@@ -9,8 +9,6 @@ var data = {
 
 var imageUrlElement = document.querySelector('#photo-url');
 var image = document.querySelector('img');
-// var titleElement = document.querySelector('#title');
-// var notesElement = document.querySelector('#notes');
 var $form = document.querySelector('form');
 
 function uploadImage(event) {
@@ -21,17 +19,35 @@ function uploadImage(event) {
 imageUrlElement.addEventListener('input', uploadImage);
 
 var dataObject = {};
-function save(event) {
+function saveEntry(event) {
   event.preventDefault();
   dataObject.title = $form.elements.title.value;
   dataObject.photo = $form.elements.photo.value;
   dataObject.notes = $form.elements.notes.value;
   dataObject.nextEntryId = data.nextEntryId++;
   data.entries.unshift(dataObject);
+  image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  var newEntry = JSON.stringify(dataObject);
+  localStorage.setItem('New Entry', newEntry);
+  var dataModel = JSON.stringify(data);
+  localStorage.setItem('Data Model', dataModel);
+  $form.reset();
   // console.log(dataObject);
   // console.log(data);
-  image.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $form.reset();
 }
 
-$form.addEventListener('submit', save);
+var newEntryJSON = localStorage.getItem('New Entry');
+
+if (newEntryJSON !== null) {
+  data.entries = JSON.parse(newEntryJSON);
+}
+
+function saveData(event) {
+  var newEntry = JSON.stringify(dataObject);
+  localStorage.setItem('New Entry', newEntry);
+  var dataModel = JSON.stringify(data);
+  localStorage.setItem('Data Model', dataModel);
+}
+
+window.addEventListener('beforeunload', saveData);
+$form.addEventListener('submit', saveEntry);
