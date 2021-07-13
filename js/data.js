52d955ba20cbf632320d6946778db204fcddce15
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-13 09:56:31 -0700

    Added the nextEntryId to the object

--- js/data.js
+++ js/data.js
@@ -6,3 +6,28 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+var imageUrlElement = document.querySelector('#photo-url');
+var image = document.querySelector('img');
+// var titleElement = document.querySelector('#title');
+// var notesElement = document.querySelector('#notes');
+var $form = document.querySelector('form');
+
+function uploadImage(event) {
+  var imageUrl = event.target.value;
+  image.setAttribute('src', imageUrl);
+}
+
+imageUrlElement.addEventListener('input', uploadImage);
+
+var dataObject = {};
+function save(event) {
+  event.preventDefault();
+  dataObject.title = $form.elements.title.value;
+  dataObject.photo = $form.elements.photo.value;
+  dataObject.notes = $form.elements.notes.value;
+  dataObject.nextEntryId = 1;
+  // console.log(dataObject);
+}
+
+$form.addEventListener('submit', save);
--- js/main.js
+++ js/main.js
@@ -1,26 +1,2 @@
 /* global data */
 /* exported data */
-
-var imageUrlElement = document.querySelector('#photo-url');
-var image = document.querySelector('img');
-// var titleElement = document.querySelector('#title');
-// var notesElement = document.querySelector('#notes');
-var $form = document.querySelector('form');
-
-function uploadImage(event) {
-  var imageUrl = event.target.value;
-  image.setAttribute('src', imageUrl);
-}
-
-imageUrlElement.addEventListener('input', uploadImage);
-
-var dataObject = {};
-function save(event) {
-  event.preventDefault();
-  dataObject.title = $form.elements.title.value;
-  dataObject.photo = $form.elements.photo.value;
-  dataObject.notes = $form.elements.notes.value;
-  // console.log(dataObject);
-}
-
-$form.addEventListener('submit', save);
