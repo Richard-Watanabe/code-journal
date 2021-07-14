09510c25da3ebd0bf355766b7be59e56085a9643
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-13 22:05:40 -0700

    Used a loop to create a DOM tree for each journal entry in the data model and append it to the pagewhen the 'DOMContentLoaded' event is fired

--- js/main.js
+++ js/main.js
@@ -28,9 +28,48 @@ function saveEntry(event) {
 
 $form.addEventListener('submit', saveEntry);
 
-function findEntry(entry) {
-  var foundEntry = document.querySelector(entry);
-  return foundEntry;
+var parent = document.querySelector('ul');
+
+function createDom(entry) {
+  var newList = document.createElement('li');
+
+  var rowDiv = document.createElement('div');
+  rowDiv.setAttribute('class', 'row');
+
+  var columnDiv = document.createElement('div');
+  columnDiv.setAttribute('class', 'column-half cover');
+
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', entry.photo);
+
+  var columnDiv2 = document.createElement('div');
+  columnDiv2.setAttribute('class', 'column-half');
+
+  var newH2 = document.createElement('h2');
+  newH2.setAttribute('class', 'less-margin');
+  newH2.textContent = entry.title;
+
+  var newP = document.createElement('p');
+  newP.textContent = entry.notes;
+
+  columnDiv2.appendChild(newH2);
+  columnDiv2.appendChild(newP);
+
+  rowDiv.appendChild(columnDiv);
+  rowDiv.appendChild(columnDiv2);
+
+  columnDiv.appendChild(newImage);
+
+  newList.appendChild(rowDiv);
+
+  return newList;
 }
 
-findEntry('.dummy2');
+function addEntry(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    var entryNew = createDom(data.entries[i]);
+    parent.appendChild(entryNew);
+  }
+}
+
+window.addEventListener('DOMContentLoaded', addEntry);
