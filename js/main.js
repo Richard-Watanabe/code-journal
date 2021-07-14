813ed5ca120e97786d95ccf4c329121f23eff2d6
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-14 10:15:44 -0700

    Ensured that refreshing the pages shows the same view as before refreshing

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ function saveEntry(event) {
   data.entries.unshift(dataObject);
   image.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
-  window.location.reload(true);
+  window.location.reload();
 }
 
 $form.addEventListener('submit', saveEntry);
