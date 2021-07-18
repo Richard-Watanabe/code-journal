5b5e99a3b61490eadd14cf88dbe00128ef220e61
Richard-Watanabe <shoryuwatanabe124@gmail.com>
2021-07-18 00:06:39 -0700

    In progress, trying to make edit happen without having to reload page

--- js/main.js
+++ js/main.js
@@ -2,73 +2,62 @@
 /* exported data */
 
 var $imageUrlElement = document.querySelector('#photo-url');
-var $imageUrlElementEdit = document.querySelector('#photo-url-edit');
-var $titleElementEdit = document.querySelector('#title-edit');
-var $notesElementEdit = document.querySelector('#notes-edit');
-var $image = document.querySelector('.entry-image');
-var $imageEdit = document.querySelector('.edit-image');
-var $formEntry = document.querySelector('.entry-form');
-var $formEdit = document.querySelector('.edit-form');
+var $titleElement = document.querySelector('#title');
+var $notesElement = document.querySelector('#notes');
+var $image = document.querySelector('img');
+var $form = document.querySelector('form');
+var $ul = document.querySelector('ul');
+var $h1 = document.querySelector('.form-title');
+var $parent = document.querySelector('ul');
+var $body = document.querySelector('body');
+var $view = document.querySelectorAll('.view');
+var $noEntry = document.querySelector('.noEntry');
+
+$imageUrlElement.addEventListener('input', uploadImage);
+$form.addEventListener('submit', saveEntry);
+window.addEventListener('DOMContentLoaded', addEntry);
+window.addEventListener('DOMContentLoaded', stayOnView);
+$ul.addEventListener('click', showEditForm);
+$body.addEventListener('click', linkSwitch);
 
 function uploadImage(event) {
   var imageUrl = event.target.value;
   $image.setAttribute('src', imageUrl);
 }
 
-function uploadImageEdit(event) {
-  var imageUrlEdit = event.target.value;
-  $imageEdit.setAttribute('src', imageUrlEdit);
-}
-
-$imageUrlElementEdit.addEventListener('input', uploadImageEdit);
-$imageUrlElement.addEventListener('input', uploadImage);
-
 function saveEntry(event) {
   event.preventDefault();
   var dataObject = {
-    title: $formEntry.elements.title.value,
-    photo: $formEntry.elements.photo.value,
-    notes: $formEntry.elements.notes.value
+    title: $form.elements.title.value,
+    photo: $form.elements.photo.value,
+    notes: $form.elements.notes.value
   };
   dataObject.entryId = data.nextEntryId;
   data.nextEntryId++;
-  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $noEntry.className = 'hidden';
-  $formEntry.reset();
   switchView('entries');
-  var addNew = createDom(dataObject);
-  $parent.prepend(addNew);
-  data.entries.unshift(dataObject);
-}
-
-function editEntry(event) {
-  event.preventDefault();
-  var dataObjectEdited = {
-    title: $formEdit.elements.title.value,
-    photo: $formEdit.elements.photo.value,
-    notes: $formEdit.elements.notes.value
-  };
-  $noEntry.className = 'hidden';
-  switchView('entries');
-  //   for (var i = 0; i < data.entries.length; i++) {
-  //     if (data.entries[i].entryId === data.editing.entryId) {
-  //       var addNew = createDom(dataObjectEdited);
-  //       $parent.prepend(addNew);
-  //       $parent.removeChild();
-  //     }
-  //   }
-  // }
-  for (var i = 0; i < data.entries.length; i++) {
-    if (data.entries[i].entryId === data.editing.entryId) {
-      data.entries.splice(i, 1, dataObjectEdited);
+  if ($h1.textContent === 'New Entry') {
+    $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+    var addNew = createDom(dataObject);
+    $parent.prepend(addNew);
+    data.entries.unshift(dataObject);
+    $noEntry.className = 'hidden';
+    $form.reset();
+  } else {
+    for (var i = 0; i < data.entries.length; i++) {
+      if (data.entries[i].entryId === data.editing.entryId) {
+        data.editing.title = $form.elements.title.value;
+        data.editing.photo = $form.elements.photo.value;
+        data.editing.notes = $form.elements.notes.value;
+        data.entries.splice(i, 1, dataObject);
+        var addEdit = createDom(data.editing);
+        // $parent.prepend(addEdit);
+        addNew.replaceWith(addEdit);
+        switchView('entries');
+        // $parent.removeChild($parent.lastElementChild);
+      }
     }
   }
 }
-
-$formEntry.addEventListener('submit', saveEntry);
-$formEdit.addEventListener('submit', editEntry);
-
-var $parent = document.querySelector('ul');
 
 function createDom(entry) {
   var newList = document.createElement('li');
@@ -91,8 +80,7 @@ function createDom(entry) {
   newH2.textContent = entry.title;
 
   var newIcon = document.createElement('i');
-  newIcon.setAttribute('class', 'fas fa-pen icon link');
-  newIcon.setAttribute('data-view', 'edit-form');
+  newIcon.setAttribute('class', 'fas fa-pen icon');
 
   var newP = document.createElement('p');
   newP.textContent = entry.notes;
@@ -105,7 +93,6 @@ function createDom(entry) {
   rowDiv.appendChild(columnDiv2);
 
   columnDiv.appendChild(newImage);
-
   newList.appendChild(rowDiv);
 
   return newList;
@@ -118,16 +105,9 @@ function addEntry(event) {
   }
 }
 
-window.addEventListener('DOMContentLoaded', addEntry);
-
 function stayOnView(event) {
   switchView(data.view);
 }
-
-window.addEventListener('DOMContentLoaded', stayOnView);
-
-var $body = document.querySelector('body');
-var $view = document.querySelectorAll('.view');
 
 function switchView(name) {
   data.view = name;
@@ -147,29 +127,23 @@ function linkSwitch(event) {
   }
 }
 
-$body.addEventListener('click', linkSwitch);
-
-var $noEntry = document.querySelector('.noEntry');
 if (data.entries.length !== 0) {
   $noEntry.className = 'hidden';
 }
 
-var $ul = document.querySelector('ul');
-
 function showEditForm(event) {
   if (event.target.matches('i')) {
+    switchView('entry-form');
     var stringId = event.target.closest('li').getAttribute('data-entry-id');
     for (var i = 0; i < data.entries.length; i++) {
       if (data.entries[i].entryId === parseInt(stringId)) {
         data.editing = data.entries[i];
       }
     }
-    $titleElementEdit.setAttribute('value', data.editing.title);
-    $imageUrlElementEdit.setAttribute('value', data.editing.photo);
-    $imageEdit.setAttribute('src', data.editing.photo);
-    $notesElementEdit.textContent = data.editing.notes;
-    switchView('edit-form');
+    $titleElement.setAttribute('value', data.editing.title);
+    $imageUrlElement.setAttribute('value', data.editing.photo);
+    $image.setAttribute('src', data.editing.photo);
+    $notesElement.textContent = data.editing.notes;
+    $h1.textContent = 'Edit Entry';
   }
 }
-
-$ul.addEventListener('click', showEditForm);
